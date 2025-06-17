fadb657c8ce5c7a74800d50cf0ea1ee52e8e109b
Demond Balentine <demond.balentine@atlasschool.com>
2025-06-16 22:44:28 -0500

    Create payments.py

---
+++ payments.py
@@ -0,0 +1,8 @@
+SUBSCRIPTION_TIERS = {
+    "bi_weekly": {"price": 15, "duration_days": 14},
+    "monthly": {"price": 30, "duration_days": 30},
+    "quarterly": {"price": 80, "duration_days": 90},
+    "semi_annually": {"price": 150, "duration_days": 180},
+    "annually": {"price": 280, "duration_days": 365},
+    "bi_annually": {"price": 500, "duration_days": 730}
+}
